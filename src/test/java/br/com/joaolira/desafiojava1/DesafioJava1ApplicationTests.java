118d052eeb60f9d64b44c361eed400079a63c62a
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-01 11:07:11 -0300

    inicia projeto

---
+++ src/test/java/br/com/joaolira/desafiojava1/DesafioJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaolira.desafiojava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
