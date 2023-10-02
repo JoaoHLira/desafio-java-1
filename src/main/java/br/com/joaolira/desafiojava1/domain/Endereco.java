fed82cf37c198b4ac068951495f683227cc2fda6
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-01 21:47:58 -0300

    deleta classe Endereco

--- src/main/java/br/com/joaolira/desafiojava1/domain/Endereco.java
+++
@@ -1,9 +0,0 @@
-package br.com.joaolira.desafiojava1.domain;
-
-public class Endereco {
-
-    private Integer cep;
-    private String cidade;
-    private String logradouro;
-    private Integer numero;
-}
