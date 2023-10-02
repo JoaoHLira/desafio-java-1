4b3d6100a1b84ffd66ace5be1964d2f0f756ae7d
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-01 21:36:18 -0300

    implementa contrato da api

---
+++ src/main/java/br/com/joaolira/desafiojava1/application/api/ClienteAPI.java
@@ -0,0 +1,14 @@
+package br.com.joaolira.desafiojava1.application.api;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/cliente")
+public interface ClienteAPI {
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRquest);
+}
