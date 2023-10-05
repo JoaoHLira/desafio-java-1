1622d40d795d12405ff26f9aae7bbc1a53fa0497
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-04 21:20:46 -0300

    implementa fluxo api, teste ok!

--- src/main/java/br/com/joaolira/desafiojava1/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/joaolira/desafiojava1/cliente/application/api/ClienteController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Log4j2
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +20,13 @@ public class ClienteController implements ClienteAPI {
         ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
         log.info("[finaliza] ClienteController - postCliente");
         return clienteCriado;
+    }
+
+    @Override
+    public List<ClienteListResponse> getTodosClientes() {
+        log.info("[inicia] ClienteController - ClienteListResponse");
+
+        log.info("[finaliza] ClienteController - ClienteListResponse");
+        return null;
     }
 }
