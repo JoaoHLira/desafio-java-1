97151fcb5eec212497ce7554d9721f2aafc287b6
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-04 21:59:17 -0300

    implementa fluxo service, teste ok!

--- src/main/java/br/com/joaolira/desafiojava1/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/joaolira/desafiojava1/cliente/application/service/ClienteService.java
@@ -1,14 +1,18 @@
 package br.com.joaolira.desafiojava1.cliente.application.service;
 
+import br.com.joaolira.desafiojava1.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.joaolira.desafiojava1.cliente.application.api.ClienteListResponse;
 import br.com.joaolira.desafiojava1.cliente.application.api.ClienteRequest;
 import br.com.joaolira.desafiojava1.cliente.application.api.ClienteResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 
 public interface ClienteService {
     ClienteResponse criaCliente(ClienteRequest clienteRequest);
 
     List<ClienteListResponse> getTodosClientes();
+
+    ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente);
 }
