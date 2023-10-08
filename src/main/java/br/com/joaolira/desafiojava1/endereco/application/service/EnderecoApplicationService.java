20cb009225ca48d56584e45ff63ecdb82184e85a
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-08 17:52:41 -0300

    implementa fluxo service, teste ok!

--- src/main/java/br/com/joaolira/desafiojava1/endereco/application/service/EnderecoApplicationService.java
+++ src/main/java/br/com/joaolira/desafiojava1/endereco/application/service/EnderecoApplicationService.java
@@ -2,7 +2,9 @@ package br.com.joaolira.desafiojava1.endereco.application.service;
 
 import br.com.joaolira.desafiojava1.cliente.application.repository.ClienteRepository;
 import br.com.joaolira.desafiojava1.cliente.domain.Cliente;
+import br.com.joaolira.desafiojava1.endereco.application.api.EnderecoAPI;
 import br.com.joaolira.desafiojava1.endereco.application.api.request.EnderecoRequest;
+import br.com.joaolira.desafiojava1.endereco.application.api.response.EnderecoListResponse;
 import br.com.joaolira.desafiojava1.endereco.application.api.response.EnderecoResponse;
 import br.com.joaolira.desafiojava1.endereco.application.repository.EnderecoRepository;
 import br.com.joaolira.desafiojava1.endereco.domain.Endereco;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Log4j2
@@ -27,5 +30,14 @@ public class EnderecoApplicationService implements EnderecoService {
         return EnderecoResponse.builder()
                 .idEndereco(endereco.getIdEndereco())
                 .build();
+    }
+
+    @Override
+    public List<EnderecoListResponse> getEnderecosPorIdCliente(UUID idCliente) {
+        log.info("[inicia] EnderecoApplicationService - buscaEnderecosPorIdCliente");
+        Cliente clientePorId = clienteRepository.buscaClienteAtravesId(idCliente);
+        List<Endereco> enderecos = enderecoRepository.buscaTodosEnderecos();
+        log.info("[finaliza] EnderecoApplicationService - buscaEnderecosPorIdCliente");
+        return EnderecoListResponse.converte(enderecos);
     }
 }
