5ae9b00f16bbfc709fa21f0c7c707eebd8304df9
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-08 14:57:34 -0300

    implementa ApplicationService

---
+++ src/main/java/br/com/joaolira/desafiojava1/endereco/application/service/EnderecoApplicationService.java
@@ -0,0 +1,31 @@
+package br.com.joaolira.desafiojava1.endereco.application.service;
+
+import br.com.joaolira.desafiojava1.cliente.application.repository.ClienteRepository;
+import br.com.joaolira.desafiojava1.cliente.domain.Cliente;
+import br.com.joaolira.desafiojava1.endereco.application.api.request.EnderecoRequest;
+import br.com.joaolira.desafiojava1.endereco.application.api.response.EnderecoResponse;
+import br.com.joaolira.desafiojava1.endereco.application.repository.EnderecoRepository;
+import br.com.joaolira.desafiojava1.endereco.domain.Endereco;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class EnderecoApplicationService implements EnderecoService {
+    private final EnderecoRepository enderecoRepository;
+    private final ClienteRepository clienteRepository;
+    @Override
+    public EnderecoResponse criaEndereco(UUID idCliente, EnderecoRequest enderecoRequest) {
+        log.info("[inicia] EnderecoApplicationService - criaEndereco");
+        Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
+        Endereco endereco = enderecoRepository.salva(new Endereco(cliente, enderecoRequest));
+        log.info("[finaliza] EnderecoApplicationService - criaEndereco");
+        return EnderecoResponse.builder()
+                .idEndereco(endereco.getIdEndereco())
+                .build();
+    }
+}
