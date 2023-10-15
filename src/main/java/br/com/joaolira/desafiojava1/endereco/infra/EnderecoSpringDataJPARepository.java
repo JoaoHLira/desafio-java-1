5e3149047bbe7625a17e8b1ffe81f4d3fb326727
JoaoHLira <lirarosas.jh@gmail.com>
2023-10-15 17:19:16 -0300

    deleta entidade endereco

--- src/main/java/br/com/joaolira/desafiojava1/endereco/infra/EnderecoSpringDataJPARepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.joaolira.desafiojava1.endereco.infra;
-
-import br.com.joaolira.desafiojava1.endereco.domain.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EnderecoSpringDataJPARepository extends JpaRepository<Endereco, Long> {
-}
